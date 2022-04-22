0a13ad064e7ba931aef76d8ebce58f115d6c4ccf
tommaso808 <65737680+tommaso808@users.noreply.github.com>
2022-04-22 16:13:52 -0500

    Create shellsetup.sh

---
+++ shellsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export VERIFY_CHECKSUM=false
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
+unzip terraform_1.0.7_linux_amd64.zip
+mkdir ~/bin
+mv terraform ~/bin
+export TF_PLUGIN_CACHE_DIR="/tmp"
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
