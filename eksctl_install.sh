131b6a6d30390360f55c4521bd7d015b4e6e44c2
tommaso808 <65737680+tommaso808@users.noreply.github.com>
2022-05-02 09:51:51 -0500

    Final submission

--- eksctl_install.sh
+++ eksctl_install.sh
@@ -8,6 +8,7 @@
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /home/cloudshell-user/bin
-eksctl utils associate-iam-oidc-provider --cluster udacity-cluster --approve --region=us-east-2
+#eksctl utils associate-iam-oidc-provider --cluster udacity-cluster --approve --region=us-east-2
 
-eksctl create iamserviceaccount --name cluster-autoscaler --namespace kube-system --cluster udacity-cluster --attach-policy-arn "arn:aws:iam::757851765448:policy/udacity-k8s-autoscale" --approve --region us-east-2
+#eksctl create iamserviceaccount --name cluster-autoscaler --namespace kube-system --cluster udacity-cluster --attach-policy-arn "arn:aws:iam::757851765448:policy/udacity-k8s-autoscale" --approve --region us-east-2
+#kubectl -n kube-system logs -f deployment/cluster-autoscaler
